24c81accf5cf184aeaf0a256c3594f3728429d7a
frankois <dabonot.francois@gmail.com>
2025-11-15 09:01:00 +0100

    fix tests

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,6 +4,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
     alias(libs.plugins.androidLibrary)
+    alias(libs.plugins.kotlinx.serialization)
 }
 
 group = "io.github.frankois944.matomoKMPTracker"
